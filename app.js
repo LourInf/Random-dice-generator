152f4f5f6c834d78c4abf775b191dc29c6ff876a
LourInf <lourdes.infante@gmail.com>
2023-10-19 11:34:46 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+//random nr between 1 and 6
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+console.log(randomNumber1);
+
+//Use the random number you created in the last step to pick
+//out a random dice image between dice1.png to dice 6.png then place this image inside the left <img> element.
+
+let randomImage = document.querySelector("img");
+console.log(randomImage);
